18c54ca887a74ed246b26c450cdd8bb3889e69ce
PedroQuaiato-ITL <pedro.quaiato@intelidata.inf.br>
2025-07-29 17:04:22 -0300

    commit geral teste

---
+++ init.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e  # Para o script se qualquer etapa falhar
+
+echo "=====> Verificando wait-for-it.sh..."
+
+# Verifica se o wait-for-it.sh existe, se não baixa
+if [ ! -f "./wait-for-it.sh" ]; then
+  echo "=====> Baixando wait-for-it.sh..."
+  curl -s -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+  chmod +x wait-for-it.sh
+else
+  echo "=====> wait-for-it.sh já existe, seguindo..."
+fi
+
+# Log de execução
+arquivo="/var/teste/log_exec.txt"
+
+if [ ! -e "$arquivo" ]; then
+  echo "Log de execuções do arquivo init.sh:" > "$arquivo"
+  echo "Arquivo criado em $(date)" >> "$arquivo"
+  echo "-----------------------------------------" >> "$arquivo"
+fi
+
+read -p "Digite seu nome: " nome
+read -p "Digite seu email: " email
+data=$(date '+%Y-%m-%d %H:%M:%S')
+
+echo "Executado por: $nome | Email: $email | Data: $data" >> "$arquivo"
+echo "-----------------------------------------" >> "$arquivo"
+
+echo "=====> Dando permissões para as pastas da aplicação"
+cd /var/teste/airflow_architecture/airflow
+sudo chmod -R 775 dags logs plugins
+
+echo "=====> Subindo Postgres e Redis..."
+docker compose -f docker-compose.yaml up -d postgres redis
+
+echo "=====> Esperando Postgres ficar pronto..."
+./wait-for-it.sh localhost:5432 -- echo "=====> Postgres OK"
+
+echo "=====> Esperando Redis ficar pronto..."
+./wait-for-it.sh localhost:6379 -- echo "=====> Redis OK"
+
+echo "=====> Rodando airflow-init..."
+docker compose -f docker-compose.yaml run --rm airflow-init
+
+echo "=====> Subindo Airflow stack..."
+docker compose -f docker-compose.yaml up -d
+
+echo "=====> Esperando Airflow Webserver ficar pronto..."
+./wait-for-it.sh localhost:8080 -- echo "=====> Airflow Webserver OK"
+
+echo "=====> Checando Worker..."
+if docker ps | grep -q docker-airflow-worker-1; then
+  echo "=====> Limpando PID do Worker..."
+  docker exec docker-airflow-worker-1 rm -f /opt/airflow/airflow-worker.pid || true
+fi
+
+echo "=====> Reiniciando Worker..."
+docker compose -f docker-compose.yaml restart airflow-worker
+
+echo "=====> Subindo Elastic..."
+cd /var/teste/infrastructure_architecture/infrastructure
+docker compose -f docker-compose-elastic.yaml up -d
+
+echo "=====> Esperando Elastic (9200)..."
+./wait-for-it.sh localhost:9200 -- echo "=====> Elastic OK"
+
+echo "=====> Subindo NGINX HUB..."
+cd /var/teste/web_architecture/hub
+docker compose -f docker-compose-nginx-hub.yaml up -d
+
+echo "=====> Subindo NGINX Vendas..."
+cd /var/teste/web_architecture/map_sales
+docker compose -f docker-compose-nginx-sales.yml up -d
+
+echo "=====> Subindo Prometheus..."
+cd /var/teste/infrastructure_architecture/infrastructure
+docker compose -f docker-compose-prometheus.yaml up -d
+
+echo "=====> FIM! Tudo rodando. Verifique com: docker ps"
